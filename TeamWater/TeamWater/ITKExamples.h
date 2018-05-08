3809d4502cba5ba3f2151508721e12e953f7f735
adu19 <adu19@users.noreply.github.com>
2018-05-08 11:02:03 -0700

    Added more options to main menu. Changed variable name from baseDicomDirectory to fixedDicomDirectory.

--- TeamWater/TeamWater/ITKExamples.h
+++ TeamWater/TeamWater/ITKExamples.h
@@ -1,7 +1,7 @@
 #ifndef _ITKEXAMPLES_H
 #define _ITKEXAMPLES_H
 
-void DicomSeriesReadImageWrite2(const char* dicomDirectory, const char* outputFileName);
+void DicomSeriesReadImageWrite2(const char* dicomDirectory, const char* outputImageFile);
 void ImageReadImageSeriesWrite(const char* inputFile, const char* outputPrefix, const char* outputExtension);
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
 
