29c578a5fec76788ea5f59c6778a8e35ba673d53
adu19 <adu19@users.noreply.github.com>
2018-05-13 03:48:30 -0700

    Added AffineRegistration.cxx, updated main program with options to choose registration method.

--- TeamWater/TeamWater/ITKExamples.h
+++ TeamWater/TeamWater/ITKExamples.h
@@ -4,5 +4,6 @@
 void DicomSeriesReadImageWrite2(const char* dicomDirectory, const char* outputImageFile);
 void ImageReadImageSeriesWrite(const char* inputFile, const char* outputPrefix, const char* outputExtension);
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
-
+void BSplineRegistration2(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
+void AffineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
 #endif
