dff8857d47eaacebcf40d12d6c30659072d2be7e
adu19 <adu19@users.noreply.github.com>
2018-05-14 00:15:45 -0700

    Removed old option 5 in main. Added region-growing and watershed segmentation.

--- TeamWater/TeamWater/ITKExamples.h
+++ TeamWater/TeamWater/ITKExamples.h
@@ -6,4 +6,8 @@ void ImageReadImageSeriesWrite(const char* inputFile, const char* outputPrefix, 
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
 void BSplineRegistration2(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
 void AffineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile, const char* differenceImageFile);
+void WatershedSegmentation1(const char* inputImageFile, const char* outputImageFile, const char* conductanceTerm, const char* diffusionIterations, 
+	const char* lowerThreshold, const char* outputScaleLevel, const char* gradientMode);
+void NeighborhoodConnectedImageFilter(const char* inputImageFile, const char* outputImageFile, const char* seedX,
+	const char* seedY, const char* lowThreshold, const char* highThreshold);
 #endif
